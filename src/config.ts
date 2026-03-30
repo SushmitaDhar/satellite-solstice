0519c72307064c87456f4e2dc18ff468ddd89b10
SushmitaDhar <chayanikadhar3@gmail.com>
2026-03-30 19:34:50 +0530

    My first edits

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Sushmita Dhar | Data Storyteller';
+export const SITE_DESCRIPTION = 'Plug-in your data and create the story';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
+
+// Add these new lines below
+export const MY_NAME = 'Sushmita Dhar';
+export const MY_TITLE = 'Data Storyteller';
